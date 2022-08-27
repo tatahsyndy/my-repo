7b16ef55b4f2a597e2795417aa4115332ef1329a
Cloud User <ec2-user@ip-172-31-47-108.us-east-2.compute.internal>
2022-08-27 22:54:11 +0000

    first commit

---
+++ hello.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "hello good morning"
+echo "today's date is"
+date
+echo "today class was indeed great"
---
+++ sc/hello.sh
@@ -0,0 +1,5 @@
+#!/bin/bask
+echo "good morning"
+echo "today is"
+date
+echo "today's class was great"
