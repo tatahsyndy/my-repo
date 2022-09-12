4c53fa303b9211cb957cc2ed3c4ff3ecafd09cc1
Cloud User <ec2-user@ip-172-31-47-108.us-east-2.compute.internal>
2022-09-12 00:48:36 +0000

    my commit filles

---
+++ java.sh
@@ -0,0 +1,6 @@
+# install Java JDK 1.8+ as a pre-requisit for tomcat to run.
+cd /opt 
+sudo yum install git wget -y
+sudo yum install java-1.8.0-openjdk-devel -y
+# Download tomcat software and extract it.
+sudo yum install wget unzip -y
